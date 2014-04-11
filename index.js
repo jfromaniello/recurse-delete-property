10717089e25d2b7138289bc4cbc4c9173f727295
José F. Romaniello <jfromaniello@gmail.com>
2014-04-11 08:43:49 -0300

    minor

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var removeProperty = module.exports = function (input, property) {
   return Object.keys(input).filter(function (p) {
     return p !== property;
-  }).reduce(function (prev, property) {
-    var value = input[property];
-    prev[property] = typeof value === 'object' ?
+  }).reduce(function (prev, p) {
+    var value = input[p];
+    prev[p] = typeof value === 'object' ?
                         removeProperty(value, property) :
                         value;
     return prev;
